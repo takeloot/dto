fe3a256f9460b6408f6c0cb92c6985fca3651762
Aleksey Tsvetkov <code.aleksey@icloud.com>
2022-10-20 20:01:49 +0300

    added work statuses enum

---
+++ src/common/enum/work-statuses.ts
@@ -0,0 +1,10 @@
+export enum EWorkStatuses {
+  IS_DEPOSIT_DISABLED = 'isDepositDisabled',
+  IS_WITHDRAWAL_DISABLED = 'isWithdrawalDisabled',
+  IS_SELL_DISABLED = 'isSellDisabled',
+  IS_MAINTENANCE = 'isMaintenance',
+  IS_STEAM_PROBLEMS = 'isSteamProblems',
+  IS_FUCKUP = 'isFuckup',
+  IS_QIWI_DISABLED = 'isQiwiDisabled',
+  IS_TINKOFF_DISABLED = 'isTinkoffDisabled',
+}
